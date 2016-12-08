bd7cecee3996896d5db994819d889bd5d42e5ff0
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-12-08 09:24:14 +0500

    Added spec for `.generate : String`

--- spec/ulid_spec.cr
+++ spec/ulid_spec.cr
@@ -1,5 +1,50 @@
 require "./spec_helper"
 
 describe ULID do
-  # TODO: Write tests
+  describe ".generate : String" do
+    it "should not raise an error" do
+      begin
+        ULID.generate
+      rescue err
+        err.should be_nil
+      end
+    end
+
+    it "should return correct length" do
+      ULID.generate.size.should eq 26
+    end
+
+    it "should contain only correct chars" do
+      (ULID.generate =~ /[^0123456789ABCDEFGHJKMNPQRSTVWXYZ]/).should be_nil
+    end
+
+    it "should be in upcase" do
+      res = ULID.generate
+
+      res.should eq res.upcase
+    end
+
+    it "should be unique" do
+      len = 1000
+      arr = [] of String
+
+      len.times do
+        arr << ULID.generate
+      end
+
+      arr.uniq!
+
+      arr.size.should eq len
+    end
+
+    it "should be sortable" do
+      1000.times do
+        ulid_1 = ULID.generate
+        sleep 1.millisecond
+        ulid_2 = ULID.generate
+
+        (ulid_2 > ulid_1).should be_true
+      end
+    end
+  end
 end
