ea3701138d626078545f2fd2ce7c37bb4e482d93
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-12-08 09:23:40 +0500

    Added ULID generator

--- src/ulid.cr
+++ src/ulid.cr
@@ -1,4 +1,44 @@
+require "secure_random"
+
 require "./ulid/*"
 
 module ULID
+  extend self
+
+  # Crockford's Base32
+  private ENCODING      = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+  private ENCODING_LEN  = ENCODING.size
+  private TIME_LEN      = 10
+  private RANDOM_LEN    = 16
+
+  # Generate ULID
+  # 
+  # ```
+  # ULID.generate
+  # # => "01B3EAF48P97R8MP9WS6MHDTZ3"
+  # ```
+  def generate : String
+    encode_time(Time.now, TIME_LEN) + encode_random(RANDOM_LEN)
+  end
+
+  private def encode_time(now : Time, len : Int32) : String
+    ms = now.epoch_ms
+
+    String.build do |io|
+      len.times do |i|
+        mod = ms % ENCODING_LEN
+        io << ENCODING[mod]
+        ms = (ms - mod) / ENCODING_LEN
+      end
+    end.reverse
+  end
+
+  private def encode_random(len : Int32) : String
+    String.build do |io|
+      len.times do |i|
+        rand = Random.rand(ENCODING_LEN)
+        io << ENCODING[rand]
+      end
+    end.reverse
+  end
 end
